8049126f966ae851683f712109e830e0d99eb605
dryilmaz <scidurmusyilmaz@gmail.com>
2019-05-01 02:26:26 +0300

    Wed May  1 02:26:26 +03 2019

--- new_work/cal.sh
+++ new_work/cal.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+IFS=$'\n'
 file=/home/dyilmaz/Desktop/shell/dolmus/new_work/yeni1
 cd /home/dyilmaz/Desktop/shell/dolmus/new_work
+#while read line
+#do
+#  echo "$line denededede "
+#done < yeni1
 for line in `cat $file`
 do
-  echo -n "Adana $line ulaşım rehberi" >> ./sonuc/$line
+  echo -e "ADANA $line ulaşım rehberi için bizi takip et." >> ./sonuc/$line
 done
