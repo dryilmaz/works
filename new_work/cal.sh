61b34099c02401fbe24ba4d5aae38f6e7c75d252
dryilmaz <scidurmusyilmaz@gmail.com>
2019-05-01 01:59:08 +0300

    Wed May  1 01:59:08 +03 2019

--- new_work/cal.sh
+++ new_work/cal.sh
@@ -3,5 +3,5 @@ file=/home/dyilmaz/Desktop/shell/dolmus/new_work/yeni1
 cd /home/dyilmaz/Desktop/shell/dolmus/new_work
 for line in `cat $file`
 do
-  echo "Adana $line ulaşım rehberi" >> ./sonuc/$line
+  echo -n "Adana $line ulaşım rehberi" >> ./sonuc/$line
 done
