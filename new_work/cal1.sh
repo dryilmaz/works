2534ef0d718a50dbec43fe87d7ea728965e88061
dryilmaz <scidurmusyilmaz@gmail.com>
2019-06-19 20:22:08 +0300

    Wed Jun 19 20:22:08 +03 2019

--- new_work/cal1.sh
+++ new_work/cal1.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-#defination
+
+##############################################
+#     defination                             #
+##############################################
 city=Ankara
 city1=ankara
 ulasim1=ankara-dolmus-saatleri
 ulasim=Ulaşım
-array=("dolmuş saatleri," "dolmuş," "dolmuş ücreti,"
+array=("ulaşım," "dolmuş saatleri," "dolmuş," "dolmuş ücreti,"
 "$city dolmuş saatleri," "minibüs saatleri," "minibüs," "$city minibüs saatleri," "minibüs ücreti,")
+array1=("dolmuş saatleri," "minibüs saatleri")
 IFS=$'\n'
 file=/home/dyilmaz/Desktop/shell/dolmus/new_work/yeni1
 cd /home/dyilmaz/Desktop/shell/dolmus/new_work
+##############################################
+#   end  defination                          #
+##############################################
+
+
+##############################################
+#     create keywords                        #
+##############################################
 for line in `cat $file`
 do
   for i in "${array[@]}"
@@ -19,6 +31,21 @@ done
 
 for line in `cat $file`
 do
+  for i in "${array1[@]}"
+  do
+    echo "$city $line $i" >> ./sonuc/"$line"
+  done
+done
+##############################################
+#    end create keywords                     #
+##############################################
+
+
+##############################################
+#    create text                             #
+##############################################
+for line in `cat $file`
+do
   echo -e " <b>$line dolmuş saatleri</b> tarafımızca takip edilmekte ve bilgi verilmektedir.
   $city $line ulaşım bilgisine belediye otobüsü saatlerine ve dolmuş saatlerine yer verilmektedir.
   sitemiz üzerinde bulunan iller kısmından tüm <b>Türkiye</b>'deki ulaşım bilgilere ulaşabilirsiniz.
@@ -26,7 +53,7 @@ do
   <a href="https://dolmussaatleri.com/$city1'_'$ulasim1/">$city $ulasim</a> sayfası üzerinden tüm ulaşım saatleri
   paylaşılmaktadır.
   <h2>$line dolmuş saatleri</h2>
-  
+
   $city aktif olan tüm dolmuş saatleri aşağıda paylaşılmaktadır.
   [catlist name=\"$city dolmus\" numberposts=-1]
   " >> ./sonuc/"$line"
@@ -40,6 +67,13 @@ do
   anlık olarak güncelleme yapılmaktadır." >> ./sonuc/"$line"
 done
 
+##############################################
+#   end create text                          #
+##############################################
+
+##############################################
+#   edit all files                           #
+##############################################
 cd /home/dyilmaz/Desktop/shell/dolmus/new_work/sonuc
 ls  | while read filename
 do
@@ -52,3 +86,6 @@ do
      ls -l "$newfile"
    fi
 done
+##############################################
+#    end  edit all files                     #
+##############################################
