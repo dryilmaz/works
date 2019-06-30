76040ebb1f117f53369284d0085b75f8bee573e0
dryilmaz <scidurmusyilmaz@gmail.com>
2019-07-01 00:57:44 +0300

    Mon Jul  1 00:57:44 +03 2019

--- new_work/cal1.sh
+++ new_work/cal1.sh
@@ -7,9 +7,9 @@ city=Ankara
 city1=ankara
 ulasim1=ankara-dolmus-saatleri
 ulasim=Ulaşım
-array=("ulaşım," "dolmuş saatleri," "dolmuş," "dolmuş ücreti,"
-"$city dolmuş saatleri," "minibüs saatleri," "minibüs," "$city minibüs saatleri," "minibüs ücreti,")
-array1=("dolmuş saatleri," "minibüs saatleri")
+#array=("ulaşım," "dolmuş saatleri," "dolmuş," "dolmuş ücreti,"
+#"$city dolmuş saatleri," "minibüs saatleri," "minibüs," "$city minibüs saatleri," "minibüs ücreti,")
+#array1=("dolmuş saatleri," "minibüs saatleri")
 IFS=$'\n'
 file=/home/dyilmaz/Desktop/shell/dolmus/new_work/yeni1
 cd /home/dyilmaz/Desktop/shell/dolmus/new_work
@@ -21,21 +21,21 @@ cd /home/dyilmaz/Desktop/shell/dolmus/new_work
 ##############################################
 #     create keywords                        #
 ##############################################
-for line in `cat $file`
-do
-  for i in "${array[@]}"
-  do
-    echo "$line $i" >> ./sonuc/"$line"
-  done
-done
-
-for line in `cat $file`
-do
-  for i in "${array1[@]}"
-  do
-    echo "$city $line $i" >> ./sonuc/"$line"
-  done
-done
+#  for line in `cat $file`
+#  do
+#    for i in "${array[@]}"
+#    do
+#      echo "$line $i" >> ./sonuc/"$line"
+#    done
+#  done
+#
+#  for line in `cat $file`
+#  do
+#    for i in "${array1[@]}"
+#    do
+#      echo "$city $line $i" >> ./sonuc/"$line"
+#    done
+#  done
 ##############################################
 #    end create keywords                     #
 ##############################################
@@ -46,25 +46,12 @@ done
 ##############################################
 for line in `cat $file`
 do
-  echo -e " <b>$line dolmuş saatleri</b> tarafımızca takip edilmekte ve bilgi verilmektedir.
-  $city $line ulaşım bilgisine belediye otobüsü saatlerine ve dolmuş saatlerine yer verilmektedir.
-  sitemiz üzerinde bulunan iller kısmından tüm <b>Türkiye</b>'deki ulaşım bilgilere ulaşabilirsiniz.
-  <b>$city $line</b> için sitemiz tarafından bulunan tüm ulaşım bilgileri yayınlanmıştır.
-  <a href="https://dolmussaatleri.com/$city1'_'$ulasim1/">$city $ulasim</a> sayfası üzerinden tüm ulaşım saatleri
-  paylaşılmaktadır.
-  <h2>$line dolmuş saatleri</h2>
-
-  $city aktif olan tüm dolmuş saatleri aşağıda paylaşılmaktadır.
-  [catlist name=\"$city dolmus\" numberposts=-1]
-  " >> ./sonuc/"$line"
-  echo "$city ulaşımı için güncellenen bilgiler sitemiz üzerinden yayınlanacaktır.
-  Yeni yapılan değişiklikleri tarafımıza bildirmeniz durumunda gerçekliği kontrol
-  edilip sitemiz güncellenecektir. <a href="https://dolmussaatleri.com/iletisim/">İletişim sayfamız</a>
-  üzerinden bizi bilgilendirebilirsiniz.
-  Ulaşım saatleri $city $line trafik durumunda göre değişiklik gösterebilir. Resmi ve dini bayramlarda
-  ücretlerde aynı şekilde değişiklik göstermektedir. Ulaşım saatlerindeki değişiklikler $city hava
-  koşuluna göse tekrar değişiklik göstermektedir. Gerekli ulaşım bilgileri tarafımızca takip edilmekte ve
-  anlık olarak güncelleme yapılmaktadır." >> ./sonuc/"$line"
+  echo -e "<b>$line dolmuş saatleri</b>"
+done
+for line in `cat $file`
+do
+  echo -e "\"title\"***\"category\"***\"text\"
+  \"$line dolmuş saatleri ve güzergahı\"***\"$city1 ulaşım\"***\"<b>$line dolmuş saatleri ve güzergahı</b> tarafımızca takip edilmekte ve bilgi verilmektedir. $city $line ulaşım bilgisine belediye otobüsü saatlerine ve dolmuş saatleri bulunmaktadır. Sitemiz üzerinde bulunan iller kısmından tüm <b>Türkiye</b>'deki ulaşım bilgilere ulaşabilirsiniz. <b>$city $line</b> için sitemiz tarafından bulunan tüm ulaşım bilgileri yayınlanmıştır. <a href="https://dolmussaatleri.com/$city1'_'$ulasim1/">$city $ulasim</a> sayfası üzerinden tüm ulaşım saatleri paylaşılmaktadır. <h2>$line dolmuş saatleri</h2> $city aktif olan tüm dolmuş saatleri aşağıda paylaşılmaktadır. [catlist name= \"$city dolmus\" numberposts=-1] Ulaşım $city için güncellenen bilgiler sitemiz üzerinden yayınlanacaktır. Dolmuş saatleri ve güzergahlarının tüm güncellemesi sitemizde paylaşılmıştır. Yeni yapılan değişiklikleri tarafımıza bildirmeniz durumunda gerçekliği kontrol edilip sitemiz güncellenecektir. <a href="https://dolmussaatleri.com/iletisim/">İletişim sayfamız</a> üzerinden bizi bilgilendirebilirsiniz. Ulaşım saatleri $city $line trafik durumunda göre değişiklik gösterebilir. Resmi ve dini bayramlarda ücretlerde aynı şekilde değişiklik göstermektedir. Ulaşım saatlerindeki değişiklikler $city hava koşuluna göse tekrar değişiklik göstermektedir. Gerekli ulaşım bilgileri tarafımızca takip edilmekte ve anlık olarak güncelleme yapılmaktadır.\"" >> ./sonuc/"$line.csv"
 done
 
 ##############################################
---
+++ new_work/key.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+city=Antalya
+echo "$1 $2 $3 hareket saatleri,"
+echo "$1 $2 $3 güzergahı,"
+echo "$2 $3 ulaşım,"
+echo "$city $2 $3"
